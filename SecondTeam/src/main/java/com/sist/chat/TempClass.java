b9463244444737594fe8ff99d838dc110559c37b
user <user@DESKTOP-L24DG0U>
2024-02-08 09:41:16 +0900

    1232

---
+++ SecondTeam/src/main/java/com/sist/chat/TempClass.java
@@ -0,0 +1,6 @@
+package com.sist.chat;
+//최초커밋적용용 임시 클래스
+public class TempClass {
+//클래스 사용시 지우고 커밋
+	
+}
---
+++ SecondTeam/src/main/java/com/sist/interceptor/TempClass.java
@@ -0,0 +1,6 @@
+package com.sist.interceptor;
+//최초커밋적용용 임시 클래스
+public class TempClass {
+//클래스 사용시 지우고 커밋
+	
+}

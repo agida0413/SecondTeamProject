4a571cfe745bb2969597dafd33920e71929d0712
YYYYYYna <officeight@naver.com>
2024-02-11 21:25:23 +0900

    정유나 vo

---
+++ SecondTeam/src/main/java/com/sist/vo/SnsKeepVO.java
@@ -0,0 +1,10 @@
+package com.sist.vo;
+
+import lombok.Data;
+
+@Data
+public class SnsKeepVO {
+	private int kano,hit;
+	private String keeploc,keeptitle,keepwriter,
+	keepregdate,keepaddr,keepimage,keepcontent;
+}
